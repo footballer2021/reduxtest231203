9980ea349a89b2f001afd7797052ed3caa77abcc
footballer2021 <soccerzuki2013@gmail.com>
2023-12-03 06:42:34 +0900

    counter complete with redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useSelector, useDispatch, TypedUseSelectorHook } from 'react-redux';
+import type { RootState, AppDispatch } from './redux/store';
+import { decrease, increase } from './redux/counterSlice';
 
-function App() {
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppDispatch = () => useDispatch<AppDispatch>(); 
+
+const App:React.FC = () => {
+  const count = useAppSelector((state) => state.counter.count);
+  const dispatch = useAppDispatch();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Hello World!</h1>
+      <h3>Count:{count}</h3>
+      <button onClick={() => dispatch(increase())}>Up</button>
+      <button onClick={() => dispatch(decrease())}>Down</button>
+    </>
   );
 }
 
